085188cd1be185c2508271cbcf590c526ebd4dc5
jurandir moura <jurandirmoura40@gmail.com>
2021-12-20 14:19:18 -0300

    inicio do projeto java

---
+++ src/com/jurandir/cursojava/aula10/Variaveis.java
@@ -0,0 +1,34 @@
+package com.jurandir.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//convencao java
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "toto";
+		String ano2014;
+		
+		//aceito mais nao ultilizado
+		int _idade;
+		int $idade;
+		
+		
+		//nao e convencao java
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+		//ma pratica
+		int a = 10;
+		String b = "Loiane";
+
+	}
+
+}
