5971e6e97419883e27d420a5d1435161608cbede
jurandir moura <jurandirmoura40@gmail.com>
2021-12-22 17:40:58 -0300

    exercicios praticados

---
+++ bin/com/jurandir/cursojava/labs/exerc05.java
@@ -0,0 +1,21 @@
+package com.jurandir.cursojava.labs;
+
+import java.util.Scanner;
+
+public class exerc05 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com a quantidade de metros: ");
+		double metros = scan.nextDouble();
+		
+		double cm = metros * 100;
+		
+		System.out.println(metros + "m e igual a " + cm + "cm");
+		
+
+	}
+
+}
