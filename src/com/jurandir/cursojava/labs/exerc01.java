063807851aee8143ec964ea8660428c63021fb4b
jurandir moura <jurandirmoura40@gmail.com>
2021-12-31 21:11:42 -0300

    realocando as pastas

---
+++ src/com/jurandir/cursojava/labs/exerc01.java
@@ -0,0 +1,12 @@
+package com.jurandir.cursojava.labs;
+
+public class exerc01 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Alo Mundo");
+
+	}
+
+}
---
+++ src/com/jurandir/cursojava/labs/exerc08.java
@@ -0,0 +1,24 @@
+package com.jurandir.cursojava.labs;
+
+import java.util.Scanner;
+
+public class exerc08 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o Valor/Hora:");
+		double valorHora = scan.nextDouble();
+		
+		System.out.println("Entre com as horas trabalhadas ao mes: ");
+		double horas = scan.nextDouble();
+		
+		double salario = valorHora * horas;
+		
+		System.out.println("Seu salario é: " + salario);
+		
+
+	}
+
+}
